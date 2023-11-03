a66b69cc5bed78ddbf6a881b53e620c1ef0b47ce
2218064 <namh2801@naver.com>
2023-11-03 15:47:28 +0900

    컴퓨터프로그래밍 7주차
    7주차

---
+++ test.py
@@ -0,0 +1,15 @@
+import matplotlib.pylot as plt
+import numpy as np
+
+data = np.random.randn(1000)
+
+plt.clf()
+
+plt.hist(data, bins=20, color='skyblue', edgecolor='black')
+
+plt.title('Histogram Chart')
+
+plt.xlabel('Values')
+plt.ylabel('Frequency')
+
+plt.savefig('./results/histogram.png')
